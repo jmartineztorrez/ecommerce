e164d50dd489e4d07b78329d43ac39bd28c104df
Victor-EA <57403468+Victor-EA@users.noreply.github.com>
2020-02-27 15:24:43 +0100

    Update models.py

--- ecommerce/sprint8/models.py
+++ ecommerce/sprint8/models.py
@@ -1,18 +1,29 @@
-from django.db import models
+from django.db import models  
+# Create your models here. 
 
-# Create your models here.
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=30,null=True)
+class Categoria(models.Model):     
+    creacion = models.DateTimeField(auto_now_add=True,auto_now=False)     
+    nombre = models.CharField(max_length=50,null=True)      
+    def __str__(self):         
+        return self.nombre   
+class Producto(models.Model):     
+    nombre = models.CharField(max_length=50,null=True)     
+    precio = models.FloatField(null=True)     
+    descripcion = models.CharField(max_length=200,null=True)     
+    categorias = models.ForeignKey(Categoria,on_delete=models.CASCADE,null=True)      
+    def __str__(self):         
+        return self.nombre  
 
-
-class Producto(models.Model):
-    creacion = models.DateTimeField(auto_now_add=True,auto_now=False)
-    nombre = models.CharField(max_length=50,null=True)
-    precio = models.FloatField(null=True)
-    ref = models.CharField(max_length=10,null=True)
-    descripcion = models.CharField(max_length=200,null=True)
-    #categoria = models.OneToManyRel(Categoria)
-
-#class Cesta
-
-#class Cliente
+class Cliente(models.Model):     
+    nombre = models.CharField(max_length=50,null=True)     
+    passw = models.CharField(max_length=50,null=True)#revisar el field de password      
+    def __str__(self):         
+        return self.nombre   
+    
+class Cesta (models.Model):     
+    cantidad =models.FloatField(null=True)     
+    total=models.FloatField(null=True)     
+    productos=models.ManyToManyField(Producto)     
+    clientes=models.ForeignKey(Cliente,on_delete=models.CASCADE,null=True)      
+    def __str__(self):         
+        return self.cantidad
