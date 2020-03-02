e51d5f8b9b471e4530a0a357331e261a005d6d7b
RodrosD <rodrigodiazsempere@hotmail.com>
2020-03-02 18:00:50 +0100

    sad

--- ecommerce/sprint8/views.py
+++ ecommerce/sprint8/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import *
 from django.urls import reverse_lazy
 from django.views.generic import View, TemplateView, ListView, UpdateView, CreateView, DeleteView
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm 
 
 
 # Create your views here.
@@ -31,3 +35,8 @@ class ListaCategoriaProducto(ListView):
         context['categoriasId']=Categoria.objects.filter(id=parametro)
         return context
         
+class RegistroUsuario(CreateView):
+    model = User
+    template_name = 'sprint8/user.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('sprint8:index')
