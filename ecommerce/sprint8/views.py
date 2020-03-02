b59f986befbef9686e1becded60f76087458dbd8
RodrosD <rodrigodiazsempere@hotmail.com>
2020-03-02 16:38:58 +0100

    <dfs

--- ecommerce/sprint8/views.py
+++ ecommerce/sprint8/views.py
@@ -11,21 +11,23 @@ class Test(ListView):
     context_object_name = 'categorias'
     queryset = Categoria.objects.all()
 
-class Cesta(TemplateView):
+class Cesta(ListView):
+    model = Categoria
     template_name = 'sprint8/cesta.html'
+    context_object_name = 'categorias'
+    queryset = Categoria.objects.all()
 
 class ListaCategoriaProducto(ListView):
     model = Categoria
     template_name = 'sprint8/listar.html'
+    context_object_name = 'categorias'
+    queryset = Categoria.objects.all()
 
-    def get_queryset(self, **kwargs):
-        parametro = self.kwargs.get('id',None)
-        queryset = Categoria.objects.filter(id=parametro) 
-        return queryset
 
     def get_context_data(self, **kwargs):
         context=super(ListaCategoriaProducto, self).get_context_data(**kwargs)
         parametro = self.kwargs.get('id', None)
         context['productos']=Producto.objects.filter(categorias=parametro)
+        context['categoriasId']=Categoria.objects.filter(id=parametro)
         return context
         
