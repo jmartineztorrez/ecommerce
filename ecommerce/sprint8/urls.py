442434e0b8e31ef6b3dadacfe622582b66d039d1
RodrosD <rodrigodiazsempere@hotmail.com>
2020-03-02 13:28:33 +0100

    ssda

--- ecommerce/sprint8/urls.py
+++ ecommerce/sprint8/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('test/', Test.as_view(), name="test"),
+    path('', Test.as_view(), name="index"),
     path('listar/<int:id>', ListaCategoriaProducto.as_view(), name="listar")
 ]
